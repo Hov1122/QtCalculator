601f0ec0935f7812880ec436b9c7f25e2ea58abd
Hov1122 <avanyanhovhannes@gmail.com>
2021-10-07 14:34:37 +0400

    initial release

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Calculator w;
+    w.setWindowIcon(QIcon(":/pre/images/Calculator-icon.png"));
     w.show();
     return a.exec();
 }
